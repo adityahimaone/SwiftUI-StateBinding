e7bbbe111100b6a3401c7e587acb34c091fb485f
adityahimaone <adityahimaone@gmail.com>
2024-01-25 23:01:40 +0700

    Initial Commit

---
+++ Sesi4StateBinding/Sesi4StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesi4StateBindingApp.swift
+//  Sesi4StateBinding
+//
+//  Created by Aditya Himawan on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesi4StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
